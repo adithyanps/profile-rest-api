9d9764f60104d942ed5eb707135966ea859680b9
ADITHYAN <adithynps3@gmial.com>
2018-12-05 14:13:56 +0530

    enabled the django admin for the user profile model

--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -2,56 +2,69 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser #is the base of the django std user model.
 from django.contrib.auth.models import PermissionsMixin #it allows us to add permissions to our user model
 from django.contrib.auth.models import BaseUserManager
-
 # Create your models here.
+# class UserProfileManager(BaseUserManager):
+#     """helps django work with custom user model """
+#     def create_user(self, email, name, password=None):
+#         """create a new user profile object"""
+#         if not email:
+#             raise ValueError('User must have an email address.')
+#         email = self.normalize_email(email)
+#         user = self.model(email=email, name=name,)
+#         user.set_password(password)
+#         user.save(using=self._db)
+#         return user
+#     def create_superuser(self, email, name ,password):
+#         """creates and saves a new superuser with given details"""
+#         user = self.create_user(email, name, password)
+#         user.is_superuser = True
+#         user.is_staff = True
+#
+#         user.save(using=self._db)
+#         return user
 class UserProfileManager(BaseUserManager):
-    """helps django work with custom user model """
+    """Helps Django work with our custom user model."""
 
-    def create_user(self, email,name,password=None):
-        """create a new user profile object"""
+    def create_user(self, email, name, password=None):
+        """Creates a new user profile."""
 
         if not email:
-            raise ValueError('User must have an email address.')
+            raise ValueError('Users must have an email address.')
 
         email = self.normalize_email(email)
-        user = self.model(email=email, name=name)
+        user = self.model(email=email, name=name,)
+
         user.set_password(password)
         user.save(using=self._db)
 
         return user
 
-    def create_superuser(self, email, name ,password):
-        """creates and saves a new superuser with given details"""
+    def create_superuser(self, email, name, password):
+        """Creates and saves a new superuser with given details."""
 
-        user = self.create_user(email,name, password)
-        user.is_superuser =True
+        user = self.create_user(email, name, password)
+
+        user.is_superuser = True
         user.is_staff = True
-
         user.save(using=self._db)
-        return user
 
+        return user
 class UserProfile(AbstractBaseUser,PermissionsMixin):
     """represents a user profile inside our system"""
-
-    email = models.EmailField(max_length=225,unique=True)
-    name = models.CharField(max_length=225)
-    is_activate = models.BooleanField(default=True)
+    email = models.EmailField(max_length=255,unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
-
     objects = UserProfileManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FILEDS = ['name']
-
+    REQUIRED_FIELDS = ['name']
     def get_full_name(self):
         """used to get a users fullname"""
-
         return self.name
     def get_short_name(self):
         """used to get a users short name"""
-
         return self.name
     def __str__(self):
         """django uses this it needs to convert the object to a string"""
-
         return self.email
